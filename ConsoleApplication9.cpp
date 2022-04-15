1ec030ed2f0c2386fd511aa0302059d935873e8b
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-04-15 20:16:06 +0500

    d

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int const SIZE = 5;
+    int const MAX_LEN = 4;
+
+    string nameStr;
+    string  elements[SIZE] = {"dsf","sdfsd","sdfsd","sdfsdf","sdfsdf"};
+
+    ofstream file;
+    cout << "Задайте имя файла ";
+    cin >> nameStr;
+
+    file.open(nameStr + ".txt");
+    for (int i = 0; i < SIZE; i++) {
+        if (elements[i].length() < MAX_LEN) {
+            file << elements[i] <<  endl;
+        }
+    }
+ 
+    file.close();
+
+    return 0;
+}
